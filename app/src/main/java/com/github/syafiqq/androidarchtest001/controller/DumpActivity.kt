b243ccf49376801491ac2506a652c9a20df3c03a
Syafiq <syafiq.rezpector@gmail.com>
2019-09-18 06:53:28 +0700

    inject class

--- app/src/main/java/com/github/syafiqq/androidarchtest001/controller/DumpActivity.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/controller/DumpActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.github.syafiqq.androidarchtest001.App
 import com.github.syafiqq.androidarchtest001.R
 import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract
 import io.reactivex.subjects.PublishSubject
@@ -12,8 +13,10 @@ import kotlinx.android.synthetic.main.activity_dump.*
 import kotlinx.android.synthetic.main.content_dump.*
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
+import javax.inject.Inject
 
 class DumpActivity : AppCompatActivity() {
+    @Inject
     var titleFactory:TitleContract? = null
 
     @SuppressLint("CheckResult")
@@ -21,6 +24,8 @@ class DumpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dump)
         setSupportActionBar(toolbar)
+
+        (application as App).component.inject(this)
 
         val c = AtomicInteger(0)
         val s = PublishSubject.create<View>()
