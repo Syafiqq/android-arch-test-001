3fe7bf0b315cae1d6baaea968b533aad4ca38d5f
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 17:16:55 +0700

    Initialize injector

--- app/src/main/java/com/github/syafiqq/androidarchtest001/controller/DetailActivity.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/controller/DetailActivity.kt
@@ -8,14 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.github.syafiqq.androidarchtest001.R
 import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract1
 import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_detail.*
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), HasAndroidInjector {
     @Inject
     lateinit var titleFactory: TitleContract1
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,4 +39,7 @@ class DetailActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    override fun androidInjector(): AndroidInjector<Any> {
+        return dispatchingAndroidInjector
+    }
 }
