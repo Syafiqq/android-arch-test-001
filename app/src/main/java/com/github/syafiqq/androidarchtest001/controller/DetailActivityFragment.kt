81a237faf79d22f99575901c6deee3eb4d7ad97b
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 17:10:01 +0700

    add button and text

--- app/src/main/java/com/github/syafiqq/androidarchtest001/controller/DetailActivityFragment.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/controller/DetailActivityFragment.kt
@@ -1,21 +1,48 @@
 package com.github.syafiqq.androidarchtest001.controller
 
-import androidx.fragment.app.Fragment
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.github.syafiqq.androidarchtest001.R
+import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract2
+import dagger.android.support.AndroidSupportInjection
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.fragment_detail.*
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class DetailActivityFragment : Fragment() {
+    @Inject
+    lateinit var titleFactory: TitleContract2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    @SuppressLint("CheckResult")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val s = PublishSubject.create<View>()
+        s.throttleFirst(500, TimeUnit.MILLISECONDS)
+            .subscribe {
+                textView2.text = titleFactory.title
+            }
+
+        button2.setOnClickListener(s::onNext)
+    }
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
     }
 }
