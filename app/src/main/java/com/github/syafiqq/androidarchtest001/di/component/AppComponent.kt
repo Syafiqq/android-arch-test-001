3177ff73c1626aa426a7e9864f0fcefadb62e4b5
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 10:32:10 +0700

    register dump activity module

--- app/src/main/java/com/github/syafiqq/androidarchtest001/di/component/AppComponent.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/di/component/AppComponent.kt
@@ -1,7 +1,13 @@
 package com.github.syafiqq.androidarchtest001.di.component
 
+import com.github.syafiqq.androidarchtest001.di.module.DumpActivityModule
 import dagger.Component
 import dagger.android.AndroidInjectionModule
 
-@Component(modules = [AndroidInjectionModule::class])
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        DumpActivityModule::class
+    ]
+)
 interface AppComponent
