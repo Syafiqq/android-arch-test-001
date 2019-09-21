b5c3edf9a8a7c8cf37740f9a02200cb6f9826d95
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 16:51:36 +0700

    modify title concrete

--- app/src/main/java/com/github/syafiqq/androidarchtest001/dump/concrete/TitleConcrete.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/dump/concrete/TitleConcrete.kt
@@ -1,11 +1,12 @@
 package com.github.syafiqq.androidarchtest001.dump.concrete
 
-import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract
+import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract1
+import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract2
 import java.util.*
 
-class TitleConcrete : TitleContract {
+class TitleConcrete(private val min: Int = 0, private val max: Int = 1) : TitleContract1, TitleContract2 {
     private val random = Random()
 
     override val title: String
-        get() = random.nextInt(100).toString()
+        get() = min.plus(random.nextInt(max - min)).toString()
 }
