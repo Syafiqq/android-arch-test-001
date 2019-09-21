b06cd4107c120f0e9cd7c18f9f2b151126ed9e8a
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 15:11:48 +0700

    fix context

--- app/src/main/java/com/github/syafiqq/androidarchtest001/di/module/AppModule.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/di/module/AppModule.kt
@@ -1,6 +1,5 @@
 package com.github.syafiqq.androidarchtest001.di.module
 
-import android.app.Application
 import android.content.Context
 import com.github.syafiqq.androidarchtest001.App
 import dagger.Module
@@ -11,19 +10,7 @@ import javax.inject.Singleton
 class AppModule {
     @Provides
     @Singleton
-    fun provideApplication(app: App): Application {
+    fun provideContext(app: App): Context {
         return app
-    }
-
-    @Provides
-    @Singleton
-    fun provideAppContext(app: App): Context {
-        return app.applicationContext
-    }
-
-    @Provides
-    @Singleton
-    fun provideBaseContext(app: App): Context {
-        return app.baseContext
     }
 }
