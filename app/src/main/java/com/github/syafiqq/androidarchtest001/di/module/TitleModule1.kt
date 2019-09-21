8729eb98754024092d89e8e52641b345f63c42e6
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 16:52:47 +0700

    add title module

--- app/src/main/java/com/github/syafiqq/androidarchtest001/di/module/TitleModule1.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/di/module/TitleModule1.kt
@@ -2,13 +2,14 @@ package com.github.syafiqq.androidarchtest001.di.module
 
 import com.github.syafiqq.androidarchtest001.dump.concrete.TitleConcrete
 import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract
+import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract1
 import dagger.Module
 import dagger.Provides
 
 @Module
-class TitleModule {
+class TitleModule1 {
     @Provides
-    fun provideTitleContract(): TitleContract {
-        return TitleConcrete(1, 10)
+    fun provideTitleContract(): TitleContract1 {
+        return TitleConcrete(11, 20)
     }
 }
--- app/src/main/java/com/github/syafiqq/androidarchtest001/di/module/TitleModule2.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/di/module/TitleModule2.kt
@@ -2,13 +2,15 @@ package com.github.syafiqq.androidarchtest001.di.module
 
 import com.github.syafiqq.androidarchtest001.dump.concrete.TitleConcrete
 import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract
+import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract1
+import com.github.syafiqq.androidarchtest001.dump.contract.TitleContract2
 import dagger.Module
 import dagger.Provides
 
 @Module
-class TitleModule {
+class TitleModule2 {
     @Provides
-    fun provideTitleContract(): TitleContract {
-        return TitleConcrete(1, 10)
+    fun provideTitleContract(): TitleContract2 {
+        return TitleConcrete(21, 30)
     }
 }
