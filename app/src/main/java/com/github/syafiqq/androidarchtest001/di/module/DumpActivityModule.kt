5d50946335d1e9c545d96f8e80ca4948f254d17a
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 13:51:21 +0700

    change to contribute injector as pro tip suggestion

--- app/src/main/java/com/github/syafiqq/androidarchtest001/di/module/DumpActivityModule.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/di/module/DumpActivityModule.kt
@@ -1,18 +1,12 @@
 package com.github.syafiqq.androidarchtest001.di.module
 
 import com.github.syafiqq.androidarchtest001.controller.DumpActivity
-import com.github.syafiqq.androidarchtest001.di.component.DumpActivitySubcomponent
-import dagger.Binds
 import dagger.Module
-import dagger.android.AndroidInjector
-import dagger.multibindings.ClassKey
-import dagger.multibindings.IntoMap
+import dagger.android.ContributesAndroidInjector
 
 
-@Module(subcomponents = [DumpActivitySubcomponent::class])
+@Module
 internal abstract class DumpActivityModule {
-    @Binds
-    @IntoMap
-    @ClassKey(DumpActivity::class)
-    internal abstract fun bindDumpAndroidInjectorFactory(factory: DumpActivitySubcomponent.Factory): AndroidInjector.Factory<*>
+    @ContributesAndroidInjector(modules = [TitleModule::class])
+    internal abstract fun contributeDumpAndroidInjector(): DumpActivity
 }
