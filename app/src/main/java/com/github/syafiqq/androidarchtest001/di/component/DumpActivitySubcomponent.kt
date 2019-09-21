36019716be787fa4065d9a9de8244505fd8e3c07
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 10:56:00 +0700

    Register title module

--- app/src/main/java/com/github/syafiqq/androidarchtest001/di/component/DumpActivitySubcomponent.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/di/component/DumpActivitySubcomponent.kt
@@ -1,10 +1,11 @@
 package com.github.syafiqq.androidarchtest001.di.component
 
 import com.github.syafiqq.androidarchtest001.controller.DumpActivity
+import com.github.syafiqq.androidarchtest001.di.module.TitleModule
 import dagger.Subcomponent
 import dagger.android.AndroidInjector
 
-@Subcomponent
+@Subcomponent(modules = [TitleModule::class])
 interface DumpActivitySubcomponent : AndroidInjector<DumpActivity> {
     @Subcomponent.Factory
     interface Factory : AndroidInjector.Factory<DumpActivity>
