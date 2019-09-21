4bd458f88edda037b203a6178f9756b08831c785
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 10:47:12 +0700

    implement android injector

--- app/src/main/java/com/github/syafiqq/androidarchtest001/App.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/App.kt
@@ -2,17 +2,30 @@ package com.github.syafiqq.androidarchtest001
 
 import android.app.Application
 import android.util.Log
+import com.github.syafiqq.androidarchtest001.di.component.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
 import timber.log.Timber
+import javax.inject.Inject
 
-class App : Application() {
+
+class App : Application(), HasAndroidInjector {
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
+        DaggerAppComponent.create().inject(this)
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         } else {
             Timber.plant(CrashReportingTree())
         }
+    }
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return dispatchingAndroidInjector
     }
 
     private class CrashReportingTree : Timber.Tree() {
