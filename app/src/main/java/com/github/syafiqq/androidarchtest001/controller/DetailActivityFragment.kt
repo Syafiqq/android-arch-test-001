f68af4a766a53c16cb89ce3b4e03558e2fd05465
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 15:47:33 +0700

    add Detail activity

---
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/controller/DetailActivityFragment.kt
@@ -0,0 +1,21 @@
+package com.github.syafiqq.androidarchtest001.controller
+
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.syafiqq.androidarchtest001.R
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class DetailActivityFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+}
