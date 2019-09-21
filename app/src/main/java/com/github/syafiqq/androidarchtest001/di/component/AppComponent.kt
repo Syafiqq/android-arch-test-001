fe0b85190dc80ea3d0c2d9857e8d2075cc2d52a3
Syafiq <syafiq.rezpector@gmail.com>
2019-09-21 10:25:44 +0700

    install android injection module

--- app/src/main/java/com/github/syafiqq/androidarchtest001/di/component/AppComponent.kt
+++ app/src/main/java/com/github/syafiqq/androidarchtest001/di/component/AppComponent.kt
@@ -1,3 +1,7 @@
 package com.github.syafiqq.androidarchtest001.di.component
 
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+
+@Component(modules = [AndroidInjectionModule::class])
 interface AppComponent
